3f922c7f37ee0fddc20c0e9389796914b9f2b2c4
Mikko Rapeli <mikko.rapeli@iki.fi>
2014-08-31 16:20:20 +0200

    via_drm.h: include linux/types.h instead of non-existing via_drmclient.h
    Fixes compiler error:
    
    drm/via_drm.h:36:27: fatal error: via_drmclient.h: No such file or directory
    
    Signed-off-by: Mikko Rapeli <mikko.rapeli@iki.fi>

--- include/uapi/drm/via_drm.h
+++ include/uapi/drm/via_drm.h
@@ -24,6 +24,7 @@
 #ifndef _VIA_DRM_H_
 #define _VIA_DRM_H_
 
+#include <linux/types.h>
 #include <drm/drm.h>
 
 /* WARNING: These defines must be the same as what the Xserver uses.
@@ -33,9 +34,6 @@
 #ifndef _VIA_DEFINES_
 #define _VIA_DEFINES_
 
-#ifndef __KERNEL__
-#include "via_drmclient.h"
-#endif
 
 #define VIA_NR_SAREA_CLIPRECTS		8
 #define VIA_NR_XVMC_PORTS               10
