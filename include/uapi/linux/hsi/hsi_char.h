95534c1e62b6c7035dfeeb9acc43b9c9751f1528
Mikko Rapeli <mikko.rapeli@iki.fi>
2014-08-31 13:19:45 +0200

    hsi_char.h: include stdint.h in userspace
    Fixes compiler error:
    
    linux/hsi/hsi_char.h:51:2: error: unknown type name ‘uint32_t’
    
    Signed-off-by: Mikko Rapeli <mikko.rapeli@iki.fi>

--- include/uapi/linux/hsi/hsi_char.h
+++ include/uapi/linux/hsi/hsi_char.h
@@ -20,9 +20,14 @@
  * 02110-1301 USA
  */
 
-
 #ifndef __HSI_CHAR_H
 #define __HSI_CHAR_H
+
+#ifdef __KERNEL__
+#include <linux/types.h>
+#else
+#include <stdint.h>
+#endif
 
 #define HSI_CHAR_MAGIC		'k'
 #define HSC_IOW(num, dtype)	_IOW(HSI_CHAR_MAGIC, num, dtype)
